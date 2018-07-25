022effb1076f5d1e4798009bc028300c2ee5692a
sronga <sr899@georgetown.edu>
2018-07-25 23:33:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |indivdog|
+      puts indivdog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
